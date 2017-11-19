2bcc0263b89fa122c477370cf8f9dcc29c91a3e0
secretRami <secret.ryuan@gmail.com>
2017-11-19 18:01:32 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+
+// const mkdir = require('mk-dirs');
+// const touch = require('write');
+// const del = require('del');
+
+// base dir setting
+const dir = {base:'./public/', before:'./public/src/', after:'./public/dist/'};
+
+gulp.task('test', function(){
+  console.log("gulp setting test_______________");
+});
+
+
+
+
+
